64db1f446b3600409bc09ddd5a31172da6f66e5c
Meltem UGURLU <meltemak8@gmail.com>
2021-10-09 17:21:17 +0300

    Add project files.

---
+++ VucutKutleEndeksHesaplama/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace VucutKutleEndeksHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Vücut kütle endeksi hesaplaması için lütfen bilgilerinizi giriniz.");
+
+            Console.WriteLine("Kilonuz :");
+            double kilo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Boyunuz :");
+            double boy = double.Parse(Console.ReadLine());
+
+            double result = kilo / (boy * boy);
+
+            if (result < 18.49)
+            {
+                Console.WriteLine("Sonuca göre İdeal kilonun altındasınız ." + result);
+            }
+            else if (  result< 25 )
+            {
+                Console.WriteLine("Sonuca göre İdeal kilodasınız." + result);
+            }
+            else if (result < 30)
+            {
+                Console.WriteLine("Sonuca göre İdeal kilonun üzerindesiniz." + result);
+            }
+            else 
+            {
+                Console.WriteLine("Sonuca göre İdeal kilonun çok çok üzerindesiniz ." + result);
+            }
+        }
+    }
+}
